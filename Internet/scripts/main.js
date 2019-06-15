0be1f6ffd8cb7ea98b64523c626a6392d624bca3
Rockysunny <sunyixuan1330@126.com>
2019-06-15 22:50:45 +0800

    change

--- Internet/scripts/main.js
+++ Internet/scripts/main.js
@@ -21,8 +21,8 @@ var districtByName = d3.map();
 d3.csv("data/mapdata.csv",function(error,chartdata){
 	console.log(chartdata);
 	chartdata.forEach(function(d,i){
-		index1.push({district:d.district,data:+d.datascore_2017})
-		index2.push({district:d.district,data:+d.internet_per_2016})
+		index1.push({district:d.district,data:+d.internet_per_2016})
+		index2.push({district:d.district,data:+d.datascore_2017})
 		index3.push({district:d.district,data:+d.length_internet_2017})
 		index4.push({district:d.district,data:+d.pop_eduction})        			
 	});
@@ -148,168 +148,168 @@ function pinchart(){
     }).addTo(L).setPin("#chinamap", {
         pushFollowers: !1});
 }
-function spiralplot(){
-	var   width = 500,
-        height = 500,
-        start = 0,
-        end = 2.25,
-        numSpirals = 2,
-        margin = {top:50,bottom:50,left:50,right:50};
+// function spiralplot(){
+// 	var   width = 500,
+//         height = 500,
+//         start = 0,
+//         end = 2.25,
+//         numSpirals = 2,
+//         margin = {top:50,bottom:50,left:50,right:50};
 
-    var theta = function(r) {
-      return numSpirals * Math.PI * r;
-    };
+//     var theta = function(r) {
+//       return numSpirals * Math.PI * r;
+//     };
 
-    // used to assign nodes color by group
-    var color = d3.scaleOrdinal(d3.schemeCategory10);
+//     // used to assign nodes color by group
+//     var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    var r = d3.min([width, height]) / 2 - 80;
+//     var r = d3.min([width, height]) / 2 - 80;
 
-    var radius = d3.scaleLinear()
-      .domain([start, end])
-      .range([80, r]);
+//     var radius = d3.scaleLinear()
+//       .domain([start, end])
+//       .range([80, r]);
 
-    var svg = d3.select("#peoplechart").append("svg")
-      .attr("width", width + margin.right + margin.left)
-      .attr("height", height + margin.left + margin.right)
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+//     var svg = d3.select("#peoplechart").append("svg")
+//       .attr("width", width + margin.right + margin.left)
+//       .attr("height", height + margin.left + margin.right)
+//       .append("g")
+//       .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var points = d3.range(start, end + 0.001, (end - start) / 1000);
+//     var points = d3.range(start, end + 0.001, (end - start) / 1000);
 
-    var spiral = d3.radialLine()
-      .curve(d3.curveCardinal)
-      .angle(theta)
-      .radius(radius);
+//     var spiral = d3.radialLine()
+//       .curve(d3.curveCardinal)
+//       .angle(theta)
+//       .radius(radius);
 
-    var path = svg.append("path")
-      .datum(points)
-      .attr("id", "spiral")
-      .attr("d", spiral)
-      .style("fill", "none")
-      .style("stroke", "steelblue");
+//     var path = svg.append("path")
+//       .datum(points)
+//       .attr("id", "spiral")
+//       .attr("d", spiral)
+//       .style("fill", "none")
+//       .style("stroke", "steelblue");
 
-    var spiralLength = path.node().getTotalLength(),
-        N = 21,
-        barWidth = (spiralLength / N) - 1;
+//     var spiralLength = path.node().getTotalLength(),
+//         N = 21,
+//         barWidth = (spiralLength / N) - 1;
     
-    d3.csv("data/popinternet",function(error,someData){
-    	var XScale = d3.scaleLinear()
-       		.domain(d3.extent(someData, function(d){
-        		return d.date;
-      		}))
-      		.range([0, spiralLength]);
+//     d3.csv("data/popinternet",function(error,someData){
+//     	var XScale = d3.scaleLinear()
+//        		.domain(d3.extent(someData, function(d){
+//         		return d.date;
+//       		}))
+//       		.range([0, spiralLength]);
 
-    // yScale for the bar height
-    var yScale = d3.scaleLinear()
-      .domain([0, d3.max(someData, function(d){
-        return d.value;
-      })])
-      .range([0, (r / numSpirals) - 70]);
+//     // yScale for the bar height
+//     var yScale = d3.scaleLinear()
+//       .domain([0, d3.max(someData, function(d){
+//         return d.value;
+//       })])
+//       .range([0, (r / numSpirals) - 70]);
 
-    svg.selectAll("rect")
-      .data(someData)
-      .enter()
-      .append("rect")
-      .attr("x", function(d,i){
+//     svg.selectAll("rect")
+//       .data(someData)
+//       .enter()
+//       .append("rect")
+//       .attr("x", function(d,i){
         
-        var linePer = XScale(d.date),
-            posOnLine = path.node().getPointAtLength(linePer),
-            angleOnLine = path.node().getPointAtLength(linePer - barWidth);
+//         var linePer = XScale(d.date),
+//             posOnLine = path.node().getPointAtLength(linePer),
+//             angleOnLine = path.node().getPointAtLength(linePer - barWidth);
       
-        d.linePer = linePer; // % distance are on the spiral
-        d.x = posOnLine.x; // x postion on the spiral
-        d.y = posOnLine.y; // y position on the spiral
+//         d.linePer = linePer; // % distance are on the spiral
+//         d.x = posOnLine.x; // x postion on the spiral
+//         d.y = posOnLine.y; // y position on the spiral
         
-        d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
+//         d.a = (Math.atan2(angleOnLine.y, angleOnLine.x) * 180 / Math.PI) - 90; //angle at the spiral position
 
-        return d.x;
-      })
-      .attr("y", function(d){
-        return d.y;
-      })
-      .attr("width", function(d){
-        return barWidth;
-      })
-      .attr("height", function(d){
-        return yScale(d.value);
-      })
-      .style("fill", function(d){return color(d.group);})
-      .style("stroke", "none")
-      .attr("transform", function(d){
-        return "rotate(" + d.a + "," + d.x  + "," + d.y + ")"; // rotate the bar
-      });
+//         return d.x;
+//       })
+//       .attr("y", function(d){
+//         return d.y;
+//       })
+//       .attr("width", function(d){
+//         return barWidth;
+//       })
+//       .attr("height", function(d){
+//         return yScale(d.value);
+//       })
+//       .style("fill", function(d){return color(d.group);})
+//       .style("stroke", "none")
+//       .attr("transform", function(d){
+//         return "rotate(" + d.a + "," + d.x  + "," + d.y + ")"; // rotate the bar
+//       });
     
-    // add date labels
-    var tF = d3.timeFormat("%b %Y"),
-        firstInMonth = {};
+//     // add date labels
+//     var tF = d3.timeFormat("%b %Y"),
+//         firstInMonth = {};
 
-    svg.selectAll("text")
-      .data(someData)
-      .enter()
-      .append("text")
-      .attr("dy", 10)
-      .style("text-anchor", "start")
-      .style("font", "10px arial")
-      .append("textPath")
-      // only add for the first of each month
-      .filter(function(d){
-        var sd = tF(d.date);
-        if (!firstInMonth[sd]){
-          firstInMonth[sd] = 1;
-          return true;
-        }
-        return false;
-      })
-      .text(function(d){
-        return tF(d.date);
-      })
-      // place text along spiral
-      .attr("xlink:href", "#spiral")
-      .style("fill", "grey")
-      .attr("startOffset", function(d){
-        return ((d.linePer / spiralLength) * 100) + "%";
-      })
+//     svg.selectAll("text")
+//       .data(someData)
+//       .enter()
+//       .append("text")
+//       .attr("dy", 10)
+//       .style("text-anchor", "start")
+//       .style("font", "10px arial")
+//       .append("textPath")
+//       // only add for the first of each month
+//       .filter(function(d){
+//         var sd = tF(d.date);
+//         if (!firstInMonth[sd]){
+//           firstInMonth[sd] = 1;
+//           return true;
+//         }
+//         return false;
+//       })
+//       .text(function(d){
+//         return tF(d.date);
+//       })
+//       // place text along spiral
+//       .attr("xlink:href", "#spiral")
+//       .style("fill", "grey")
+//       .attr("startOffset", function(d){
+//         return ((d.linePer / spiralLength) * 100) + "%";
+//       })
 
 
-    var tooltip = d3.select("#chart")
-    .append('div')
-    .attr('class', 'tooltip');
+//     var tooltip = d3.select("#chart")
+//     .append('div')
+//     .attr('class', 'tooltip');
 
-    tooltip.append('div')
-    .attr('class', 'date');
-    tooltip.append('div')
-    .attr('class', 'value');
+//     tooltip.append('div')
+//     .attr('class', 'date');
+//     tooltip.append('div')
+//     .attr('class', 'value');
 
-    svg.selectAll("rect")
-    .on('mouseover', function(d) {
+//     svg.selectAll("rect")
+//     .on('mouseover', function(d) {
 
-        tooltip.select('.date').html("Date: <b>" + d.date.toDateString() + "</b>");
-        tooltip.select('.value').html("Value: <b>" + Math.round(d.value*100)/100 + "<b>");
+//         tooltip.select('.date').html("Date: <b>" + d.date.toDateString() + "</b>");
+//         tooltip.select('.value').html("Value: <b>" + Math.round(d.value*100)/100 + "<b>");
 
-        d3.select(this)
-        .style("fill","#FFFFFF")
-        .style("stroke","#000000")
-        .style("stroke-width","2px");
+//         d3.select(this)
+//         .style("fill","#FFFFFF")
+//         .style("stroke","#000000")
+//         .style("stroke-width","2px");
 
-        tooltip.style('display', 'block');
-        tooltip.style('opacity',2);
+//         tooltip.style('display', 'block');
+//         tooltip.style('opacity',2);
 
-    })
-    .on('mousemove', function(d) {
-        tooltip.style('top', (d3.event.layerY + 10) + 'px')
-        .style('left', (d3.event.layerX - 25) + 'px');
-    })
-    .on('mouseout', function(d) {
-        d3.selectAll("rect")
-        .style("fill", function(d){return color(d.group);})
-        .style("stroke", "none")
+//     })
+//     .on('mousemove', function(d) {
+//         tooltip.style('top', (d3.event.layerY + 10) + 'px')
+//         .style('left', (d3.event.layerX - 25) + 'px');
+//     })
+//     .on('mouseout', function(d) {
+//         d3.selectAll("rect")
+//         .style("fill", function(d){return color(d.group);})
+//         .style("stroke", "none")
 
-        tooltip.style('display', 'none');
-        tooltip.style('opacity',0);
-    });
-    })
-}
+//         tooltip.style('display', 'none');
+//         tooltip.style('opacity',0);
+//     });
+//     })
+// }
 function makebarchart(){
   //准备画布大小
   var margin = {top: 40, right: 20, bottom: 30, left: 40},
@@ -369,3 +369,4 @@ function makebarchart(){
 }
 pinchart();
 makebarchart();
+spiralplot();
